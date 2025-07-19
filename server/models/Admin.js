70a169c0d54d187783a374227dc357cf12ef4227
M-Charan <228a1a4252@risekrishnasaiprakasam.edu.in>
2025-07-19 11:54:21 +0530

    Delete server directory

--- server/models/Admin.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const  adminSchema = new mongoose.Schema({
-    uname: {type:String},
-    password : {type : String},
-    promotedRestaurants : [{type : String}]
-})
-
-export default mongoose.model("Admin",adminSchema);
